901a81db8e9a64255e93b74e94c04c9911689c6f
ddimkou <ddimkou@outlook.com>
2023-12-28 12:56:30 +0200

    working without fetch

--- src/Trivia.jsx
+++ src/Trivia.jsx
@@ -1,22 +1,32 @@
-import { fetchQuestions } from "./api";
-import { useEffect, useState } from "react";
+// import { fetchQuestions } from "./api";
+import { useState } from "react";
+import { questionsFile } from "./questions";
 
 const Trivia = () => {
-  const [questions, setQuestions] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchQuestions();
-        setQuestions(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchData();
-  }, []);
+  const [questions, setQuestions] = useState(questionsFile);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await fetchQuestions();
+  //       setQuestions(data);
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
-  return <div>Trivia</div>;
+  return (
+    <div>
+      {questions.map((question, index) => (
+        <div key={index}>
+          <p>{question.question}</p>
+          {/* Render options and other details here */}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Trivia;
---
+++ src/questions.js
@@ -0,0 +1,103 @@
+export const questionsFile = [
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Books",
+    question: "Which Russian author wrote the epic novel War and Peace?",
+    correct_answer: "Leo Tolstoy",
+    incorrect_answers: [
+      "Fyodor Dostoyevsky",
+      "Alexander Pushkin",
+      "Vladimir Nabokov",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Entertainment: Music",
+    question:
+      "Who designed the album cover for True Romance, an album by Estelle?",
+    correct_answer: "Rebecca Sugar",
+    incorrect_answers: ["Matt Burnett", "Ian Jones Quartey", "Ben Leven"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Video Games",
+    question:
+      "In &quot;Resident Evil 2&quot;, what is Leon Kennedy&#039;s middle name?",
+    correct_answer: "Scott",
+    incorrect_answers: ["Shaun", "Simon", "Sam"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "History",
+    question:
+      "Which of the following Presidents of the United States was assassinated?",
+    correct_answer: "William McKinley",
+    incorrect_answers: [
+      "Lyndon Johnson",
+      "Chester Arthur",
+      "Franklin Roosevelt",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Vehicles",
+    question: "What country was the Trabant 601 manufactured in?",
+    correct_answer: "East Germany",
+    incorrect_answers: ["Soviet Union", "Hungary", "France"],
+  },
+  {
+    type: "multiple",
+    difficulty: "easy",
+    category: "Entertainment: Video Games",
+    question: "In what year was Hearthstone released?",
+    correct_answer: "2014",
+    incorrect_answers: ["2011", "2013", "2012"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Entertainment: Music",
+    question: "Where was Nicki Minaj born?",
+    correct_answer: "Trinidad and Tobago",
+    incorrect_answers: ["Haiti", "Saint Lucia", "Grenada"],
+  },
+  {
+    type: "multiple",
+    difficulty: "hard",
+    category: "Entertainment: Japanese Anime &amp; Manga",
+    question:
+      "What is the name of the school in the anime and manga &quot;Gosick&quot;?",
+    correct_answer: "St. Marguerite",
+    incorrect_answers: ["St. Augustine", "St. Mary", "St. Bernadette"],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "General Knowledge",
+    question: "What is a &quot;dakimakura&quot;?",
+    correct_answer: "A body pillow",
+    incorrect_answers: [
+      "A Chinese meal, essentially composed of fish",
+      "A yoga posture",
+      "A word used to describe two people who truly love each other",
+    ],
+  },
+  {
+    type: "multiple",
+    difficulty: "medium",
+    category: "Entertainment: Video Games",
+    question:
+      "When was Chapter 1 of the Source Engine mod &quot;Underhell&quot; released?",
+    correct_answer: "September 1st, 2013",
+    incorrect_answers: [
+      "March 3rd, 2011",
+      "September 12th, 2013",
+      "October 2nd, 2012",
+    ],
+  },
+];
