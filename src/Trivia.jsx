f03ad398985a6a8a3c411cc35f3a90bb3db6b3c5
ddimkou <ddimkou@outlook.com>
2023-12-28 13:51:28 +0200

    shuffle and display options

--- src/Trivia.jsx
+++ src/Trivia.jsx
@@ -17,14 +17,27 @@ const Trivia = () => {
   //   fetchData();
   // }, []);
 
+  const count = 0;
+
+  const correctAnswer = questions[count].correct_answer;
+  const incorrectAnswers = questions[count].incorrect_answers;
+  const options = [...incorrectAnswers, correctAnswer];
+
+  // shuffle with sort
+  // https://www.freecodecamp.org/news/how-to-shuffle-an-array-of-items-using-javascript-or-typescript/
+  const shuffle = (options) => {
+    return options.sort(() => Math.random() - 0.5);
+  };
+  const shuffledOptions = shuffle(options);
   return (
-    <div>
-      {questions.map((question, index) => (
-        <div key={index}>
-          <p>{question.question}</p>
-          {/* Render options and other details here */}
-        </div>
-      ))}
+    <div className="game">
+      <p>{questions[count].question}</p>
+      <ul>
+        {shuffledOptions.map((shuffleOption, key) => (
+          <li key={key}>{shuffleOption}</li>
+        ))}
+      </ul>
+      <p>correct: {correctAnswer}</p>
     </div>
   );
 };
