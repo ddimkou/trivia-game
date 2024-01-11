7ab845e472cd0f8d2fdf80e5adcbe88383e2a60e
ddimkou <ddimkou@outlook.com>
2024-01-11 21:36:03 +0200

    menu styling

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,11 +4,11 @@ import "./Menu.css";
 const Menu = ({ count, score, useHint }) => {
   return (
     <div className="menu">
-      <p>Score: {score}</p>
-      <p>{count + 1}/10</p>
       <button className="button-secondary" onClick={useHint}>
         Hint
       </button>
+      <p>Score: {score}</p>
+      <p>{count + 1}/10</p>
     </div>
   );
 };
