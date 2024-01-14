8633eb5769c507adf0c499eb964c3d650b74f8d8
ddimkou <ddimkou@outlook.com>
2024-01-14 23:43:48 +0200

    rules toggle

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,15 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
+
 const LandingPage = ({ setDifficulty }) => {
+  const [showRules, setShowRules] = useState(false);
+
   const HandleDifficulty = (e) => {
     setDifficulty(e.target.textContent);
+  };
+
+  const toggleRules = () => {
+    setShowRules(!showRules);
   };
 
   return (
@@ -18,19 +26,23 @@ const LandingPage = ({ setDifficulty }) => {
           hard
         </li>
       </ul>
-      <div className="game-rules">
-        <h3>How to Play</h3>
-        <p>Welcome to the trivia game!</p>
-        <ul>
-          <li>Select a difficulty to start the game.</li>
-          <li>Answer each question to the best of your ability.</li>
-          <li>
-            Use hints if you are stuck, but be careful - they cost points!
-          </li>
-          <li>Your score will be calculated based on correct answers.</li>
-          <li>Try to achieve the highest score possible!</li>
-        </ul>
-      </div>
+      <button className="toggle-button" onClick={toggleRules}>
+        {showRules ? "Hide Rules" : "Show Rules"}
+      </button>
+      {showRules && (
+        <div className="game-rules">
+          <p>Welcome to the trivia game!</p>
+          <ul>
+            <li>Select a difficulty to start the game.</li>
+            <li>Answer each question to the best of your ability.</li>
+            <li>
+              Use hints if you are stuck, but be careful - they cost points!
+            </li>
+            <li>Your score will be calculated based on correct answers.</li>
+            <li>Try to achieve the highest score possible!</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
